5f42a58bbc2e40669ac401688261b25cad619a95
SMHUDS1 <smhuds1@ilstu.edu>
2025-04-07 17:48:45 -0500

    Update TaskManager.java

--- BrickByBrick/src/main/java/com/it326/BrickByBrick/TaskManager.java
+++ BrickByBrick/src/main/java/com/it326/BrickByBrick/TaskManager.java
@@ -3,6 +3,7 @@ package com.it326.BrickByBrick;
 public class TaskManager {
 
     private Database db;
+    private List<Task> tasks;
     public TaskManager(Database db) {
         this.db = db;
     }
