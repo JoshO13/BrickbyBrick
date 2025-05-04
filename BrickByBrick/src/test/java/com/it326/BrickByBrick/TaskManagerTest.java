e1411d13e0c0a284dc815e192ad5f42d56810b84
SMHUDS1 <smhuds1@ilstu.edu>
2025-05-04 15:36:14 -0500

    Update TaskManagerTest.java

--- BrickByBrick/src/test/java/com/it326/BrickByBrick/TaskManagerTest.java
+++ BrickByBrick/src/test/java/com/it326/BrickByBrick/TaskManagerTest.java
@@ -1,22 +1,24 @@
 package com.it326.BrickByBrick;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.lang.reflect.Array;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
 public class TaskManagerTest {
-    
+    Database db;
+
     @Test
-    void testCreateTask() {
-       TaskManager tm = new TaskManager();
+    void testCreateTask() throws SQLException {
+        TaskManager tm = new TaskManager();
         Date date = new Date();
-        Task testTask = tm.createTask("test Task", 10, date, 10);
+        tm.createTask("test Task", date, 10, 10);
         List<Task> tasks = tm.getTasks();
-        tasks.add(testTask);
+        tasks.add(tm.searchTaskName("test Task"));
         for (Task t : tasks) {
-            assertEquals(testTask.getName(), t.getName());
+            assertEquals("test Task", t.getName());
         }
     }
 }
